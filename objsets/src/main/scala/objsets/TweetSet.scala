ce0b643b5745b6606e5eecd2138461d90790455b
Zishuo Xu <xuzishuo1996@gmail.com>
2022-01-09 19:25:09 -0500

    wrong impl of goole and apple tweets because of misuse of API

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -223,14 +223,14 @@ object GoogleVsApple:
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = allTweets.filter(tweet => tweet.text.contains(word => google.exists(word)))
+  lazy val appleTweets: TweetSet = allTweets.filter(tweet => tweet.text.contains(word => apple.exists(word)))
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 
 object Main extends App:
   // Print the trending tweets
